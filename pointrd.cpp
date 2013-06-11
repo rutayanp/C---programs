1542516283691f30dc094d775fffde3a4f7def85
rutayanp <rutayanp@gmail.com>
2013-06-11 10:36:16 -0400

    points are stored in a point object in a vector

--- pointrd.cpp
+++ pointrd.cpp
@@ -11,6 +11,9 @@
 #include <CGAL/copy_n.h>
 #include <CGAL/random_selection.h>
 #include <stdlib.h>
+
+#define DEBUG 1
+
 using namespace std;
 using namespace CGAL;
 
@@ -19,13 +22,36 @@ typedef Point_2<R>                       Point;
 typedef Creator_uniform_2<double,Point>  Creator;
 typedef std::vector<Point>               Vector;
 
+class points_2d {
+	private:
+		double x;
+		double y;
+
+	public:
+		void setValue(double i, double j)
+		{
+			x = i;
+			y = j;
+			if(DEBUG == 0) 
+				cout<<x<<","<<y<<"\n";
+		}
+
+		void printVal()
+		{
+			cout<<x<<","<<y<<"\n";
+		}
+};
+
 int main(int argc, char *argv[])
 {
 	Vector points;
-
+	vector<points_2d> mypoints;
+	points_2d *p; //to insert points into the vector
 	int in_size = atof(argv[1]);
 	points.reserve(in_size);
-	fstream fout;
+	mypoints.reserve(in_size);
+	ofstream fout;
+	ifstream fin;
 	
 	fout.open("/Users/rutayanpatro/iBlob/points.txt",ios::out|ios::trunc); 
 	
@@ -33,9 +59,50 @@ int main(int argc, char *argv[])
 	CGAL::copy_n( g, in_size, std::back_inserter(points));
 	for (int i=0; i < in_size; i++){	
 		fout<<points[i].x()<<","<<points[i].y()<<"\n";
-		cout<<points[i].x()<<","<<points[i].y()<<"\n"; 
+		if(DEBUG == 1)
+			cout<<points[i].x()<<","<<points[i].y()<<"\n"; 
 	}
-
+	
+	if(DEBUG == 1) 
+		cout<<"------------\n";
 	fout.close();
+
+	fin.open("/Users/rutayanpatro/iBlob/points.txt",ios::in);
+	
+	string x,y;
+	int temp=0,i=0;
+	string line,token;
+	stringstream iss;
+	while ( getline(fin, line) )
+	{
+		iss << line;
+		while ( getline(iss, token, ',') )
+		{
+			if(temp ==0){
+				x= token;
+				temp = 1;
+			}else{
+				y=token;
+				temp = 0;
+			}
+			
+			if(temp == 0){
+				if(DEBUG == 0) cout<<x<<","<<y<<"\n";
+				p = new points_2d;
+				p->setValue(atof(x.c_str()),atof(y.c_str()));
+				
+				mypoints.push_back(*p);
+				
+			}
+		}
+		iss.clear();
+	
+	}
+	if(DEBUG == 1){
+		for(i=0;i<in_size; i++){
+	        	mypoints[i].printVal();
+		}
+	}
+	fin.close();
 }
 
